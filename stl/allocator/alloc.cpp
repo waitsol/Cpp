a46e29171369272f50a6a44d1230d0412c43210e
wangming <1559556218@qq.com>
2022-02-20 19:33:13 +0800

    alloc

--- stl/allocator/alloc.cpp
+++ stl/allocator/alloc.cpp
@@ -1,4 +1,183 @@
-#include"common.h"
+#include "common.h"
+#include "alloc.h"
+
+template <int inst>
+typename __malloc_alloc_template<inst>::malloc_alloc_oom_handler //返回值类型 需要typename表示是类型
+    __malloc_alloc_template<inst>::__malloc_alloc_oom_handler = 0;
+
+template <int inst>
+void *__malloc_alloc_template<inst>::oom_malloc(size_t n)
+{
+    malloc_alloc_oom_handler handler = __malloc_alloc_oom_handler;
+    void *result;
+    for (;;)
+    {
+        if (0 == handler)
+        {
+            __THROW_BAD_ALLLOC;
+        }
+        handler();
+        result = malloc(n);
+        if (result)
+            return result;
+    }
+}
+template <int inst>
+void *__malloc_alloc_template<inst>::oom_realloc(void *src, size_t n)
+{
+    malloc_alloc_oom_handler handler = __malloc_alloc_oom_handler;
+    void *result;
+    for (;;)
+    {
+        if (0 == handler)
+        {
+            __THROW_BAD_ALLLOC;
+        }
+        handler();
+        result = realloc(src, n);
+        if (result)
+            return result;
+    }
+}
+typedef __malloc_alloc_template<0> malloc_alloc;
+template <bool threads, int inst>
+char *__default_alloc_template<threads, inst>::start_free = 0;
+
+template <bool threads, int inst>
+char *__default_alloc_template<threads, inst>::end_free = 0;
+
+template <bool threads, int inst>
+size_t __default_alloc_template<threads, inst>::heap_size = 0;
+
+template <bool threads, int inst>
+typename __default_alloc_template<threads, inst>::obj *volatile __default_alloc_template<threads, inst>::free_list[NFREELISTS] = {0};
+
+template <bool threads, int inst>
+void *__default_alloc_template<threads, inst>::allocate(size_t n)
+{
+    obj *result;
+    obj *volatile *my_free_list;
+    if (n > MAX_BYTES)
+    {
+        return malloc_alloc::allocate(n);
+    }
+    my_free_list = free_list + FREELIST_INDEX(n);
+    result = *my_free_list; //取n对应的内存链表 =free_list[FREELIST_INDEX(n)]
+    if (my_free_list == nullptr)
+    {
+        void *r = refile(RUOND_UP(n));
+        return r;
+    }
+    *my_free_list = result->free_list_link; //内存链表下移
+    return result;
+}
+template <bool threads, int inst>
+void *__default_alloc_template<threads, inst>::dealloacate(void *p, size_t n)
+{
+    obj *q = (obj *)p;
+    obj *volatile *my_free_list;
+    if (n > MAX_BYTES)
+    {
+        return malloc_alloc::deallocate(p,n);
+    }
+    my_free_list = free_list + FREELIST_INDEX(n);
+
+    q->free_list_link = *my_free_list;
+    *my_free_list = q;
+}
+template <bool threads, int inst>
+void *__default_alloc_template<threads, inst>::refile(size_t n)
+{
+    int nobj = 20;
+    char *chunk = chunk_alloc(n, nobj);
+
+    obj *volatile *my_free_list;
+    obj *result;
+    obj *cur_obj, next_obj;
+
+    if (nobj == 1)
+    {
+        //内存池只剩下一个了，链表不用更新
+        return chunk;
+    }
+    my_free_list = free_list + FREELIST_INDEX(n);
+    result = (obj *)chunk;
+    //前n个字节返还  指针后移，剩下的维护到链表
+    *my_free_list = next_obj = (obj *)(chunk + n);
+    for (int i = 1; i < nobj; i++)
+    {
+        cur_obj = next_obj;
+
+        if (i + 1 == nobj)
+        {
+            cur_obj->free_list_link = 0;
+        }
+        else
+        {
+            //维护链表 指针继续后移
+            next_obj = (obj *)(chunk + n);
+            cur_obj->free_list_link = next_obj;
+        }
+    }
+    return result;
+}
+template <bool threads, int inst>
+char *__default_alloc_template<threads, inst>::chunk_alloc(size_t n, int &nobj)
+{
+    size_t total_size = n * nobj;
+    size_t left_size = end_free - start_free;
+    char *result;
+    if (left_size >= total_size)
+    {
+        result = start_free;
+        start_free += total_size;
+        return result;
+    }
+    else if (left_size >= n)
+    {
+        nobj = left_size / n;
+        result = start_free;
+        start_free += nobj * n;
+        return result;
+    }
+    else
+    {
+        size_t byte_to_get = 2 * total_size + RUOND_UP(heap_size >> 4);
+        if (left_size > 0)
+        {
+            //将剩余的内存维护到链表
+            obj *volatile *my_free_list = my_free_list = free_list + FREELIST_INDEX(left_size);
+
+            ((obj *)start_free)->free_list_link = *my_free_list;
+            *my_free_list = (obj *)start_free;
+        }
+        start_free = (char *)malloc(byte_to_get);
+        if (start_free == nullptr)
+        {
+            obj *volatile *my_free_list, *p;
+            //看看n后面的节点有没有剩余
+            for (int i = n; i < MAX_BYTES; i += ALIGN)
+            {
+                my_free_list = free_list + FREELIST_INDEX(i);
+                p = *my_free_list;
+                if (*my_free_list != nullptr)
+                {
+                    *my_free_list = p->free_list_link;
+                    start_free = (char *)p;
+                    end_free = start_free + i;
+                    return chunk_alloc(n, nobj);
+                }
+            }
+            end_free = 0;
+            //看看oom机制
+            start_free = (char *)::malloc_alloc::allocate(byte_to_get);
+        }
+        heap_size += byte_to_get;
+        end_free = start_free + byte_to_get;
+        //内存申请好了 继续递归 返还nobj
+        return chunk_alloc(n, nobj);
+    }
+}
 int main()
 {
     return 0;
---
+++ stl/allocator/alloc.h
@@ -0,0 +1,101 @@
+#ifndef ALLOC_H
+#define ALLOC_H
+#include "common.h"
+
+#if 0
+    #include<new>
+    #define __THROW_BAD_ALLLOC throw bad_alloc
+#elif !defined(__THROW_BAD_ALLLOC)
+    #include <iostream>
+    #define __THROW_BAD_ALLLOC std::cerr<<"out of memory"<<std::endl;exit(-1);
+#endif
+
+
+//一级适配器
+template<int inst>
+class __malloc_alloc_template{
+public:
+    using malloc_alloc_oom_handler = void (*)();
+public:
+    ~__malloc_alloc_template (){}
+public : static void *oom_malloc(size_t);
+    static void *oom_realloc(void *, size_t);
+    static malloc_alloc_oom_handler __malloc_alloc_oom_handler;
+    // static void (*__malloc_alloc_oom_handler)();
+
+public:
+        static void *allocate(size_t n)
+        {
+            void *result=malloc(n);
+            if(0==result) result = oom_malloc(n);
+            return result;
+        }
+        static void deallocate(void *p, size_t n)
+        {
+            free(p);
+        }
+       static void *reallocate(void *p, size_t n)
+       {
+           void *result = realloc(p, n);
+           if (0 == result)
+               result = oom_realloc(p, n);
+           return result;
+       } 
+      static malloc_alloc_oom_handler set_malloc_alloc_oom_handler(malloc_alloc_oom_handler hander)
+      {
+          malloc_alloc_oom_handler old = __malloc_alloc_oom_handler;
+          __malloc_alloc_oom_handler = hander;
+          return old;
+      }
+};
+
+
+
+//二级适配器
+#define ALIGN (8)
+#define MAX_BYTES (128)
+#define NFREELISTS (MAX_BYTES / ALIGN)
+
+template<bool threads,int inst>
+class __default_alloc_template{
+public:
+private:
+    static size_t RUOND_UP(size_t bytes)
+    {
+        //很巧妙 如果有余数 +align-1 必上升 然后清除掉add后的余数即口
+        return (bytes+ALIGN-1)& ~(ALIGN-1);  //将byttes上升至ALIGN的整数倍 
+    }
+    union obj{
+        union obj* free_list_link;//不为空的时候 client保证了足够的大小，这个指针指向了下一块内存
+        char client_data[0]; //动态扩容写法 没有使用到 只是为了预留内存空间
+    };
+
+    static size_t FREELIST_INDEX(size_t bytes) 
+    {
+        // 1-ALIGN一组   
+        return (bytes - 1)/ALIGN; //
+        // stl源码写法 挺有意思  想让ALIGN和 1-ALIGn-1 一组
+        //所以+Align-1导致 1-Align都上升 维度Align没有  最后-1就行
+        // return （bytes-1+Align)/align -1;
+    }
+
+    static void *refile(size_t n);
+    //nobj 传入传出参数 代表需要的数量和实际返还的数量
+    static char* chunk_alloc(size_t n,int &nobj);
+
+
+    //state
+    static char *start_free;//内存迟位置标识
+    static char* end_free;
+    static size_t heap_size;
+    //静态变量 共享 所以没考虑析构 程序结束就释放了  但是我测试发现
+    static obj *volatile free_list[NFREELISTS];
+
+public:
+    static void* allocate(size_t n);
+    static void* dealloacate(void* p,size_t n);
+    // ...没讲好像也没用到 就没实现
+    static void *reallocate(void *p, size_t old_sz, size_t new_sz);
+};
+
+#endif
