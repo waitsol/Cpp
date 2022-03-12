3e37d800d8d24f7988d0f8c141043ff1712f6fb8
Your Name <you@example.com>
2022-03-12 18:39:13 +0800

    初版 list

--- common/common.h
+++ common/common.h
@@ -3,24 +3,28 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/time.h>
 #include <stddef.h>
 #include <sys/types.h>
 #include <stdio.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <stdlib.h>
 #include <vector>
-#include <arpa/inet.h>
-#include <unistd.h>
 #include <string.h>
 #include <iostream>
-#include <sys/epoll.h>
+
 #include <unordered_set>
 #include <thread>
 #include <functional>
 #include <fcntl.h>
+#ifndef WIN32
+#include <sys/epoll.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#endif // !WIN32
+
+
 #define ERR(condition)                        \
     do                                        \
     {                                         \
--- stl/list/list_stl.h
+++ stl/list/list_stl.h
@@ -1,5 +1,5 @@
-#ifndef __LIST_H
-#define __LIST_H
+#ifndef __LIST_H_
+#define __LIST_H_
 #include "alloc.h"
 //list节点
 template<class T>
@@ -73,9 +73,10 @@ struct __list_iterator
 };
 
 template <class T, class Alloc = alloc>
-    class list_stl : public  __list_iterator<T>{
+class list_stl : public  __list_iterator<T>{
 protected:
     typedef __list_node<T> list_node;
+	typedef simple_alloc<list_node, Alloc> list_node_allocator;
 public:
     typedef list_node* link_type;
 
@@ -101,9 +102,61 @@ public:
     {
         return *(--end());
     }
+	void swap(list_stl<T, alloc> &x)
+	{
+		std::swap(x.node, node);
+	}
+public:
+	void push_back(const T&x) { insert(end(), x); }
+	iterator insert(iterator position, const T& x)
+	{
+		link_type t = create_node(x);
+		t->next = position.node;
+		t->prev = position.noee->prev;
+		position.noee->prev->next = t;
+		position.noee->prev = t;
+		return t;
+	}
+	iterator earse(iterator position)
+	{
+		link_type next_node = position.node->next;
+		link_type prev_node = position.node->prev;
 
+		prev_node->next = next_node;
+		next_node->prev = prev_node;
+		destory_node(position.node);
+
+
+		return t;
+	}
+	bool empty()
+	{
+		return size() == 0;
+	}
+protected:
+	link_type get_node() { return list_node_allocator::allocate(); }
+	void put_node(link_type p) { list_node_allocator::deallocate(p); }
+	link_type create_node(const T& v)
+	{
+		link_type p = get_node();
+		construct(&p->data, x);
+		return x;
+	}
+	void destory_node(link_type p)
+	{
+		destory(&p->data);
+		put_node(p);
+	}
+
+	void transfer(iterator position, iterator first, iterator last);
+	void splice(iterator positionm, list_stl &x);
+	void splice(iterator positionm, list_stl &x, iterator i);
+	void splice(iterator positionm, list_stl &x,iterator first,iterator last);
+	void merge(list_stl<T, Alloc> &x);
+	void sort();
 private:
     link_type node;
-}
+};
 
 #endif
+
